b91e17e2a0299faee4ad515d1d1c11b43571d3d5
cissou <bouraikramm@gmail.com>
2024-09-20 16:38:39 +0200

    modifications

---
+++ fraction.java
@@ -0,0 +1,13 @@
+public class fraction {
+
+    // Constructeur vide (pour l'instant)
+    public fraction() {
+        // Aucun code spécifique
+    }
+
+    // Surcharge de la méthode toString pour renvoyer un message spécifique
+    @Override
+    public String toString() {
+        return "Je suis une fraction.";
+    }
+}
---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        // Crée une instance de Fraction
+        fraction fraction = new fraction();
+
+        // Appelle la méthode toString et affiche le résultat
+        System.out.println(fraction.toString());  // Cela affichera "Je suis une fraction."
+    }
+}
