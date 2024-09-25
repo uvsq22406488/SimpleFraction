f6196b98be5514ec5d6d2b40fab83416d9ff6290
cissouu <abdessatarbourai@gmail.com>
2024-09-25 14:58:34 +0200

    Ajout des méthodes d'addition, d'égalité et de comparaison dans la classe fraction

--- fraction.java
+++ fraction.java
@@ -1,10 +1,10 @@
-public class fraction {
+public class fraction extends Number {
     private int numerateur;   // Attribut représentant le numérateur
     private int denominateur; // Attribut représentant le dénominateur
 
     // Constructeur par défaut
     public fraction() {
-        this.numerateur = 0;
+        this.numerateur = 0; // Initialisation à 0
         this.denominateur = 1; // On initialise à 1 pour éviter la division par zéro
     }
 
@@ -42,10 +42,48 @@ public class fraction {
         return denominateur;
     }
 
-    // Méthode pour obtenir la valeur sous forme d'un nombre à virgule flottante
+    // Méthode pour additionner deux fractions
+    public fraction add(fraction other) {
+        int newNumerateur = this.numerateur * other.denominateur + other.numerateur * this.denominateur;
+        int newDenominateur = this.denominateur * other.denominateur;
+        return new fraction(newNumerateur, newDenominateur);
+    }
+
+    // Redéfinition de la méthode equals pour comparer deux fractions
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        fraction other = (fraction) obj;
+        return this.numerateur * other.denominateur == other.numerateur * this.denominateur;
+    }
+
+    // Méthode pour comparer les fractions
+    @Override
+    public int compareTo(fraction other) {
+        double thisValue = this.doubleValue();
+        double otherValue = other.doubleValue();
+        return Double.compare(thisValue, otherValue);
+    }
+
+    // Méthodes abstraites de Number
+    @Override
     public double doubleValue() {
         return (double) numerateur / denominateur;
     }
 
-    // Autres méthodes à ajouter plus tard (addition, soustraction, etc.)
+    @Override
+    public float floatValue() {
+        return (float) doubleValue();
+    }
+
+    @Override
+    public int intValue() {
+        return numerateur / denominateur;
+    }
+
+    @Override
+    public long longValue() {
+        return (long) doubleValue();
+    }
 }
--- main.java
+++ main.java
@@ -2,8 +2,19 @@ public class main {
     public static void main(String[] args) {
         // Crée une instance de Fraction
         fraction fraction = new fraction();
+        System.out.println(fraction.toString());  // Cela affichera "Je suis une fraction : 0/1"
 
-        // Appelle la méthode toString et affiche le résultat
-        System.out.println(fraction.toString());  // Cela affichera "Je suis une fraction."
+        // Tests pour vérifier le bon fonctionnement des méthodes
+        fraction f1 = new fraction(1, 2);
+        fraction f2 = new fraction(1, 3);
+        fraction result = f1.add(f2);
+        System.out.println(result.toString()); // Affiche "Je suis une fraction : 5/6"
+
+        // Vérification de l'égalité
+        fraction f3 = new fraction(2, 4); // Équivaut à 1/2
+        System.out.println(f1.equals(f3)); // Devrait afficher "true"
+
+        // Comparaison
+        System.out.println(f1.compareTo(f2)); // Devrait afficher un nombre positif (1/2 > 1/3)
     }
 }
